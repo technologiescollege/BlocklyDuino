502cc51e48e25a796d05770baff689f21224dc9d
SebCanet <sebastien.canet@ac-nantes.fr>
2014-11-12 02:43:27 +0100

    Revert "MàJ"
    This reverts commit ba63503b527120eff221a2434bfcf8e190bab083.

---
+++ generators/arduino/base.js
@@ -0,0 +1,317 @@
+/**
+ * Visual Blocks Language
+ *
+ * Copyright 2012 Fred Lin.
+ * https://github.com/gasolin/BlocklyDuino
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Helper functions for generating Arduino blocks.
+ * @author gasolin@gmail.com (Fred Lin)
+ */
+'use strict';
+
+//To support syntax defined in http://arduino.cc/en/Reference/HomePage
+
+//define blocks
+if (!Blockly.Language) Blockly.Language = {};
+
+Blockly.Language.base_delay = {
+  category: 'Contrôle',
+  helpUrl: 'http://arduino.cc/en/Reference/delay',
+  init: function() {
+    this.setColour(120);
+    this.appendValueInput("DELAY_TIME", Number)
+        .appendTitle("délai")
+        .setCheck(Number);
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setTooltip('Spécifier un temps d attente');
+  }
+};
+
+Blockly.Language.base_map = {
+  category: 'Maths',
+  helpUrl: 'http://arduino.cc/en/Reference/map',
+  init: function() {
+    this.setColour(230);
+    this.appendValueInput("NUM", Number)
+        .appendTitle("faire la proportionnelle ")
+        .setCheck(Number);
+    this.appendValueInput("DMAX", Number)
+        .appendTitle("sur un intervalle de [0-")
+        .setCheck(Number);
+    this.appendDummyInput("")
+	      .appendTitle("]");
+    this.setInputsInline(true);
+    this.setOutput(true);
+    this.setTooltip('transfère une valeur sur une échelle de 0 à 1023');
+  }
+};
+
+Blockly.Language.inout_buildin_led = {
+   category: 'Entrées/Sorties',
+   helpUrl: 'http://arduino.cc/en/Reference/DigitalWrite',
+   init: function() {
+     this.setColour(190);
+     this.appendDummyInput("")
+	       .appendTitle("état de la DEL sur la carte")
+	       .appendTitle(new Blockly.FieldDropdown([["HIGH", "HIGH"], ["LOW", "LOW"]]), "STAT");
+     this.setPreviousStatement(true, null);
+     this.setNextStatement(true, null);
+     this.setTooltip('éteint ou allume la DEL sur la carte Arduino');
+   }
+};
+
+Blockly.Language.inout_digital_write = {
+  category: 'Entrées/Sorties',
+  helpUrl: 'http://arduino.cc/en/Reference/DigitalWrite',
+  init: function() {
+    this.setColour(230);
+    this.appendDummyInput("")
+	      .appendTitle("mettre la broche Digital")
+	      .appendTitle(new Blockly.FieldDropdown(profile.default.digital), "PIN")
+      	.appendTitle("à l'état logique")
+      	.appendTitle(new Blockly.FieldDropdown([["1", "HIGH"], ["0", "LOW"]]), "STAT");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setTooltip('écrire un état numérique 0 ou 1 sur une sortie spécifique');
+  }
+};
+
+Blockly.Language.inout_digital_read = {
+  category: 'Entrées/Sorties',
+  helpUrl: 'http://arduino.cc/en/Reference/DigitalRead',
+  init: function() {
+    this.setColour(230);
+    this.appendDummyInput("")
+	      .appendTitle("l'état logique de la broche Digital")
+	      .appendTitle(new Blockly.FieldDropdown(profile.default.digital), "PIN");
+    this.setOutput(true, Boolean);
+    this.setTooltip('');
+  }
+};
+
+Blockly.Language.inout_analog_write = {
+  category: 'Entrées/Sorties',
+  helpUrl: 'http://arduino.cc/en/Reference/AnalogWrite',
+  init: function() {
+    this.setColour(230);
+    this.appendDummyInput("")
+        .appendTitle("écrire sur la broche Analogique")
+        .appendTitle(new Blockly.FieldDropdown(profile.default.analog), "PIN");
+    this.appendValueInput("NUM", Number)
+        .appendTitle("la valeur")
+        .setCheck(Number);
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setTooltip('envoyer une valeur comprise entre 0 et 255 sur une sortie spécifique');
+  }
+};
+
+Blockly.Language.inout_analog_read = {
+  category: 'Entrées/Sorties',
+  helpUrl: 'http://arduino.cc/en/Reference/AnalogRead',
+  init: function() {
+    this.setColour(230);
+    this.appendDummyInput("")
+        .appendTitle("la valeur lue sur la broche Analogique")
+        .appendTitle(new Blockly.FieldDropdown(profile.default.analog), "PIN");
+    this.setOutput(true, Number);
+    this.setTooltip('Return value between 0 and 1024');
+  }
+};
+
+Blockly.Language.inout_highlow = {
+  category: 'Entrées/Sorties',
+  helpUrl: 'http://arduino.cc/en/Reference/Constants',
+  init: function() {
+    this.setColour(230);
+    this.appendDummyInput("")
+        .appendTitle(new Blockly.FieldDropdown([["HIGH", "HIGH"], ["LOW", "LOW"]]), 'BOOL')
+    this.setOutput(true, Boolean);
+    this.setTooltip(Blockly.LANG_LOGIC_BOOLEAN_TOOLTIP_1);
+  }
+};
+
+//servo block
+//http://www.seeedstudio.com/depot/emax-9g-es08a-high-sensitive-mini-servo-p-760.html?cPath=170_171
+Blockly.Language.servo_move = {
+  category: 'Servo-moteur',
+  helpUrl: 'http://www.arduino.cc/playground/ComponentLib/servo',
+  init: function() {
+    this.setColour(190);
+    this.appendDummyInput("")
+        .appendTitle("faire tourner le servo-moteur")
+        .appendTitle(new Blockly.FieldImage("http://www.seeedstudio.com/depot/images/product/a991.jpg", 64, 64))
+        .appendTitle("sur la broche")
+        .appendTitle(new Blockly.FieldDropdown(profile.default.digital), "PIN")
+    this.appendValueInput("DEGREE", Number)
+        .setCheck(Number)
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendTitle("d'un angle de (0~180°)");
+    this.appendValueInput("DELAY_TIME", Number)
+        .setCheck(Number)
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendTitle("pendant un délai de");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setTooltip('rotation possible entre 0~180 degrés');
+  }
+};
+
+Blockly.Language.servo_read_degrees = {
+  category: 'Servo-moteur',
+  helpUrl: 'http://www.arduino.cc/playground/ComponentLib/servo',
+  init: function() {
+    this.setColour(190);
+    this.appendDummyInput("")
+        .appendTitle("l'angle du servo-moteur")
+        .appendTitle(new Blockly.FieldImage("http://www.seeedstudio.com/depot/images/product/a991.jpg", 64, 64))
+        .appendTitle("connecté sur la broche")
+        .appendTitle(new Blockly.FieldDropdown(profile.default.digital), "PIN");
+    this.setOutput(true, Number);
+    this.setTooltip('renvoie le nombre de degrés de la dernière rotation');
+  }
+};
+
+Blockly.Language.serial_print = {
+  category: 'Entrées/Sorties',
+  helpUrl: 'http://www.arduino.cc/en/Serial/Print',
+  init: function() {
+    this.setColour(230);
+    this.appendValueInput("CONTENT", String)
+        .appendTitle("envoi sur le port série la donnée :");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setTooltip('envoies des données sur le port série pour sruvaillance par le moniteur en ASCII');
+  }
+};
+
+// define generators
+Blockly.Arduino = Blockly.Generator.get('Arduino');
+
+Blockly.Arduino.base_delay = function() {
+  var delay_time = Blockly.Arduino.valueToCode(this, 'DELAY_TIME', Blockly.Arduino.ORDER_ATOMIC) || '1000'
+  var code = 'delay(' + delay_time + ');\n';
+  return code;
+};
+
+Blockly.Arduino.base_map = function() {
+  var value_num = Blockly.Arduino.valueToCode(this, 'NUM', Blockly.Arduino.ORDER_NONE);
+  var value_dmax = Blockly.Arduino.valueToCode(this, 'DMAX', Blockly.Arduino.ORDER_ATOMIC);
+  var code = 'map('+value_num+', 0, 1024, 0, '+value_dmax+')';
+  return [code, Blockly.Arduino.ORDER_NONE];
+};
+
+Blockly.Arduino.inout_buildin_led = function() {
+  var dropdown_stat = this.getTitleValue('STAT');
+  Blockly.Arduino.setups_['setup_output_13'] = 'pinMode(13, OUTPUT);';
+  var code = 'digitalWrite(13,'+dropdown_stat+');\n'
+  return code;
+};
+
+Blockly.Arduino.inout_digital_write = function() {
+  var dropdown_pin = this.getTitleValue('PIN');
+  var dropdown_stat = this.getTitleValue('STAT');
+  Blockly.Arduino.setups_['setup_output_'+dropdown_pin] = 'pinMode('+dropdown_pin+', OUTPUT);';
+  var code = 'digitalWrite('+dropdown_pin+','+dropdown_stat+');\n'
+  return code;
+};
+
+Blockly.Arduino.inout_digital_read = function() {
+  var dropdown_pin = this.getTitleValue('PIN');
+  Blockly.Arduino.setups_['setup_input_'+dropdown_pin] = 'pinMode('+dropdown_pin+', INPUT);';
+  var code = 'digitalRead('+dropdown_pin+')';
+  return [code, Blockly.Arduino.ORDER_ATOMIC];
+};
+
+Blockly.Arduino.inout_analog_write = function() {
+  var dropdown_pin = this.getTitleValue('PIN');
+  //var dropdown_stat = this.getTitleValue('STAT');
+  var value_num = Blockly.Arduino.valueToCode(this, 'NUM', Blockly.Arduino.ORDER_ATOMIC);
+  //Blockly.Arduino.setups_['setup_output'+dropdown_pin] = 'pinMode('+dropdown_pin+', OUTPUT);';
+  var code = 'analogWrite('+dropdown_pin+','+value_num+');\n';
+  return code;
+};
+
+Blockly.Arduino.inout_analog_read = function() {
+  var dropdown_pin = this.getTitleValue('PIN');
+  //Blockly.Arduino.setups_['setup_input_'+dropdown_pin] = 'pinMode('+dropdown_pin+', INPUT);';
+  var code = 'analogRead('+dropdown_pin+')';
+  return [code, Blockly.Arduino.ORDER_ATOMIC];
+};
+
+Blockly.Arduino.inout_highlow = function() {
+  // Boolean values HIGH and LOW.
+  var code = (this.getTitleValue('BOOL') == 'HIGH') ? 'HIGH' : 'LOW';
+  return [code, Blockly.Arduino.ORDER_ATOMIC];
+};
+
+/*
+//servo
+#include <Servo.h>
+
+Servo servo_11;
+
+void setup() { 
+  servo_11.attach(11);
+}
+
+void loop() { 
+servo_11.write(0);
+delay(2000);
+
+servo_11.write(150); //0~180
+delay(2000);
+}
+*/
+Blockly.Arduino.servo_move = function() {
+  var dropdown_pin = this.getTitleValue('PIN');
+  var value_degree = Blockly.Arduino.valueToCode(this, 'DEGREE', Blockly.Arduino.ORDER_ATOMIC);
+  //value_degree = value_degree.replace('(','').replace(')','')
+  var delay_time = Blockly.Arduino.valueToCode(this, 'DELAY_TIME', Blockly.Arduino.ORDER_ATOMIC) || '1000'
+  //delay_time = delay_time.replace('(','').replace(')','');
+  
+  Blockly.Arduino.definitions_['define_servo'] = '#include <Servo.h>\n';
+  Blockly.Arduino.definitions_['var_servo'+dropdown_pin] = 'Servo servo_'+dropdown_pin+';\n';
+  Blockly.Arduino.setups_['setup_servo_'+dropdown_pin] = 'servo_'+dropdown_pin+'.attach('+dropdown_pin+');\n';
+  
+  var code = 'servo_'+dropdown_pin+'.write('+value_degree+');\n'+'delay(' + delay_time + ');\n';
+  return code;
+};
+
+Blockly.Arduino.servo_read_degrees = function() {
+  var dropdown_pin = this.getTitleValue('PIN');
+  
+  Blockly.Arduino.definitions_['define_servo'] = '#include &lt;Servo.h&gt;\n';
+  Blockly.Arduino.definitions_['var_servo'+dropdown_pin] = 'Servo servo_'+dropdown_pin+';\n';
+  Blockly.Arduino.setups_['setup_servo_'+dropdown_pin] = 'servo_'+dropdown_pin+'.attach('+dropdown_pin+');\n';
+  
+  var code = 'servo_'+dropdown_pin+'.read()';
+  return code;
+};
+
+Blockly.Arduino.serial_print = function() {
+  var content = Blockly.Arduino.valueToCode(this, 'CONTENT', Blockly.Arduino.ORDER_ATOMIC) || '0'
+  //content = content.replace('(','').replace(')','');
+  
+  Blockly.Arduino.setups_['setup_serial_'+profile.default.serial] = 'Serial.begin('+profile.default.serial+');\n';
+  
+  var code = 'Serial.print('+content+');\nSerial.print(\'\\t\');\n';
+  return code;
+};
--- language/fr/_messages.js
+++ language/fr/_messages.js
@@ -36,9 +36,9 @@ Blockly.MSG_EXTERNAL_INPUTS = 'Variables externes';
 Blockly.MSG_INLINE_INPUTS = 'Variables fixes';
 Blockly.MSG_DELETE_BLOCK = 'Supprimer ce bloc';
 Blockly.MSG_DELETE_X_BLOCKS = 'Effacer %1 blocs';
-Blockly.MSG_COLLAPSE_BLOCK = 'Reduire le bloc';
+Blockly.MSG_COLLAPSE_BLOCK = 'Réduire le bloc';
 Blockly.MSG_EXPAND_BLOCK = 'Restaurer le bloc';
-Blockly.MSG_DISABLE_BLOCK = 'Desactiver le bloc';
+Blockly.MSG_DISABLE_BLOCK = 'Désactiver le bloc';
 Blockly.MSG_ENABLE_BLOCK = 'Activer le bloc';
 Blockly.MSG_HELP = 'Aide';
 
@@ -51,7 +51,7 @@ Blockly.MSG_RENAME_VARIABLE_TITLE = 'Renommer les "%1" variables en :';
 
 // Toolbox.
 Blockly.MSG_VARIABLE_CATEGORY = 'Variables';
-Blockly.MSG_PROCEDURE_CATEGORY = 'Procedures';
+Blockly.MSG_PROCEDURE_CATEGORY = 'Procédures';
 
 // Colour Blocks.
 Blockly.LANG_CATEGORY_COLOUR = 'Couleur';
@@ -63,19 +63,19 @@ Blockly.LANG_COLOUR_RGB_TITLE = 'colore avec';
 Blockly.LANG_COLOUR_RGB_RED = 'rouge';
 Blockly.LANG_COLOUR_RGB_GREEN = 'vert';
 Blockly.LANG_COLOUR_RGB_BLUE = 'bleu';
-Blockly.LANG_COLOUR_RGB_TOOLTIP = 'Creer une couleur avec les valeurs exactes de rouge, vert\n' +
-    'et bleu.  Toutes les valeurs doivent etre comprises entre 0,0 et 1,0.';
+Blockly.LANG_COLOUR_RGB_TOOLTIP = 'Créer une couleur avec les valeurs exactes de rouge, vert\n' +
+    'et bleu.  Toutes les valeurs doivent être comprises entre 0,0 et 1,0.';
 
-Blockly.LANG_COLOUR_BLEND_TITLE = 'melange';
+Blockly.LANG_COLOUR_BLEND_TITLE = 'mélange';
 Blockly.LANG_COLOUR_BLEND_COLOUR1 = 'couleur 1';
 Blockly.LANG_COLOUR_BLEND_COLOUR2 = 'couleur 2';
 Blockly.LANG_COLOUR_BLEND_RATIO = 'ratio';
-Blockly.LANG_COLOUR_BLEND_TOOLTIP = 'Mélange deux coleurs ensemble avec un ratio (0,0 - 1,0).';
+Blockly.LANG_COLOUR_BLEND_TOOLTIP = 'Mélange deux couleurs ensemble avec un ratio (0,0 - 1,0).';
 
 // Control Blocks.
 Blockly.LANG_CATEGORY_CONTROLS = 'Contrôle';
 Blockly.LANG_CONTROLS_IF_HELPURL = 'http://code.google.com/p/blockly/wiki/If_Then';
-Blockly.LANG_CONTROLS_IF_TOOLTIP_1 = 'If a value is true, then do some statements.';
+Blockly.LANG_CONTROLS_IF_TOOLTIP_1 = 'Si une valeur est vraie, on progresse dans les instructions.';
 Blockly.LANG_CONTROLS_IF_TOOLTIP_2 = 'If a value is true, then do the first block of statements.\n' +
     'Otherwise, do the second block of statements.';
 Blockly.LANG_CONTROLS_IF_TOOLTIP_3 = 'If the first value is true, then do the first block of statements.\n' +
@@ -92,48 +92,48 @@ Blockly.LANG_CONTROLS_IF_IF_TITLE_IF = 'si';
 Blockly.LANG_CONTROLS_IF_IF_TOOLTIP = 'Add, remove, or reorder sections\n' +
     'to reconfigure this if block.';
 
-Blockly.LANG_CONTROLS_IF_ELSEIF_TITLE_ELSEIF = 'else if';
+Blockly.LANG_CONTROLS_IF_ELSEIF_TITLE_ELSEIF = 'sinon si';
 Blockly.LANG_CONTROLS_IF_ELSEIF_TOOLTIP = 'Add a condition to the if block.';
 
-Blockly.LANG_CONTROLS_IF_ELSE_TITLE_ELSE = 'else';
+Blockly.LANG_CONTROLS_IF_ELSE_TITLE_ELSE = 'sinon';
 Blockly.LANG_CONTROLS_IF_ELSE_TOOLTIP = 'Add a final, catch-all condition to the if block.';
 
 Blockly.LANG_CONTROLS_REPEAT_HELPURL = 'http://en.wikipedia.org/wiki/For_loop';
-Blockly.LANG_CONTROLS_REPEAT_TITLE_REPEAT = 'repeter';
+Blockly.LANG_CONTROLS_REPEAT_TITLE_REPEAT = 'répéter';
 Blockly.LANG_CONTROLS_REPEAT_TITLE_TIMES = 'fois';
 Blockly.LANG_CONTROLS_REPEAT_INPUT_DO = 'faire';
 Blockly.LANG_CONTROLS_REPEAT_TOOLTIP = 'Do some statements several times.';
 
 Blockly.LANG_CONTROLS_WHILEUNTIL_HELPURL = 'http://code.google.com/p/blockly/wiki/Repeat';
-Blockly.LANG_CONTROLS_WHILEUNTIL_TITLE_REPEAT = 'repeat';
-Blockly.LANG_CONTROLS_WHILEUNTIL_INPUT_DO = 'do';
-Blockly.LANG_CONTROLS_WHILEUNTIL_OPERATOR_WHILE = 'while';
-Blockly.LANG_CONTROLS_WHILEUNTIL_OPERATOR_UNTIL = 'until';
+Blockly.LANG_CONTROLS_WHILEUNTIL_TITLE_REPEAT = 'répéter';
+Blockly.LANG_CONTROLS_WHILEUNTIL_INPUT_DO = 'faire';
+Blockly.LANG_CONTROLS_WHILEUNTIL_OPERATOR_WHILE = 'tant que';
+Blockly.LANG_CONTROLS_WHILEUNTIL_OPERATOR_UNTIL = 'jusqu à';
 Blockly.LANG_CONTROLS_WHILEUNTIL_TOOLTIP_WHILE = 'While a value is true, then do some statements.';
 Blockly.LANG_CONTROLS_WHILEUNTIL_TOOLTIP_UNTIL = 'While a value is false, then do some statements.';
 
 Blockly.LANG_CONTROLS_FOR_HELPURL = 'http://en.wikipedia.org/wiki/For_loop';
-Blockly.LANG_CONTROLS_FOR_INPUT_WITH = 'count with';
+Blockly.LANG_CONTROLS_FOR_INPUT_WITH = 'compter par';
 Blockly.LANG_CONTROLS_FOR_INPUT_VAR = 'x';
-Blockly.LANG_CONTROLS_FOR_INPUT_FROM = 'from';
-Blockly.LANG_CONTROLS_FOR_INPUT_TO = 'to';
-Blockly.LANG_CONTROLS_FOR_INPUT_DO = 'do';
+Blockly.LANG_CONTROLS_FOR_INPUT_FROM = 'de';
+Blockly.LANG_CONTROLS_FOR_INPUT_TO = 'à';
+Blockly.LANG_CONTROLS_FOR_INPUT_DO = 'faire';
 Blockly.LANG_CONTROLS_FOR_TOOLTIP = 'Count from a start number to an end number.\n' +
     'For each count, set the current count number to\n' +
     'variable "%1", and then do some statements.';
 
 Blockly.LANG_CONTROLS_FOREACH_HELPURL = 'http://en.wikipedia.org/wiki/For_loop';
-Blockly.LANG_CONTROLS_FOREACH_INPUT_ITEM = 'for each item';
+Blockly.LANG_CONTROLS_FOREACH_INPUT_ITEM = 'pour chaque item';
 Blockly.LANG_CONTROLS_FOREACH_INPUT_VAR = 'x';
-Blockly.LANG_CONTROLS_FOREACH_INPUT_INLIST = 'in list';
-Blockly.LANG_CONTROLS_FOREACH_INPUT_DO = 'do';
+Blockly.LANG_CONTROLS_FOREACH_INPUT_INLIST = 'dans la liste';
+Blockly.LANG_CONTROLS_FOREACH_INPUT_DO = 'faire';
 Blockly.LANG_CONTROLS_FOREACH_TOOLTIP = 'For each item in a list, set the item to\n' +
     'variable "%1", and then do some statements.';
 
 Blockly.LANG_CONTROLS_FLOW_STATEMENTS_HELPURL = 'http://en.wikipedia.org/wiki/Control_flow';
-Blockly.LANG_CONTROLS_FLOW_STATEMENTS_INPUT_OFLOOP = 'of loop';
-Blockly.LANG_CONTROLS_FLOW_STATEMENTS_OPERATOR_BREAK = 'break out';
-Blockly.LANG_CONTROLS_FLOW_STATEMENTS_OPERATOR_CONTINUE = 'continue with next iteration';
+Blockly.LANG_CONTROLS_FLOW_STATEMENTS_INPUT_OFLOOP = 'de boucle';
+Blockly.LANG_CONTROLS_FLOW_STATEMENTS_OPERATOR_BREAK = 'sortie';
+Blockly.LANG_CONTROLS_FLOW_STATEMENTS_OPERATOR_CONTINUE = 'continuer avec la prochaine itération';
 Blockly.LANG_CONTROLS_FLOW_STATEMENTS_TOOLTIP_BREAK = 'Break out of the containing loop.';
 Blockly.LANG_CONTROLS_FLOW_STATEMENTS_TOOLTIP_CONTINUE = 'Skip the rest of this loop, and\n' +
     'continue with the next iteration.';
@@ -142,7 +142,7 @@ Blockly.LANG_CONTROLS_FLOW_STATEMENTS_WARNING = 'Warning:\n' +
     'be used within a loop.';
 
 // Logic Blocks.
-Blockly.LANG_CATEGORY_LOGIC = 'Logic';
+Blockly.LANG_CATEGORY_LOGIC = 'Logique';
 Blockly.LANG_LOGIC_COMPARE_HELPURL = 'http://en.wikipedia.org/wiki/Inequality_(mathematics)';
 Blockly.LANG_LOGIC_COMPARE_TOOLTIP_EQ = 'Return true if both inputs equal each other.';
 Blockly.LANG_LOGIC_COMPARE_TOOLTIP_NEQ = 'Return true if both inputs are not equal to each other.';
@@ -178,7 +178,7 @@ Blockly.LANG_LOGIC_NULL_TOOLTIP = 'Returns null.';
 // Math Blocks.
 Blockly.LANG_CATEGORY_MATH = 'Maths';
 Blockly.LANG_MATH_NUMBER_HELPURL = 'http://en.wikipedia.org/wiki/Number';
-Blockly.LANG_MATH_NUMBER_TOOLTIP = 'A number.';
+Blockly.LANG_MATH_NUMBER_TOOLTIP = 'Un nombre.';
 
 Blockly.LANG_MATH_ARITHMETIC_HELPURL = 'http://en.wikipedia.org/wiki/Arithmetic';
 Blockly.LANG_MATH_ARITHMETIC_TOOLTIP_ADD = 'Return the sum of the two numbers.';
@@ -191,7 +191,7 @@ Blockly.LANG_MATH_ARITHMETIC_TOOLTIP_POWER = 'Return the first number raised to\
 Blockly.LANG_MATH_CHANGE_HELPURL = 'http://en.wikipedia.org/wiki/Negation';
 Blockly.LANG_MATH_CHANGE_TITLE_CHANGE = 'change';
 Blockly.LANG_MATH_CHANGE_TITLE_ITEM = 'item';
-Blockly.LANG_MATH_CHANGE_INPUT_BY = 'by';
+Blockly.LANG_MATH_CHANGE_INPUT_BY = 'par';
 Blockly.LANG_MATH_CHANGE_TOOLTIP = 'Add a number to variable "%1".';
 
 Blockly.LANG_MATH_SINGLE_HELPURL = 'http://en.wikipedia.org/wiki/Square_root';
@@ -239,9 +239,9 @@ Blockly.LANG_MATH_ONLIST_TOOLTIP_STD_DEV = 'Return the standard deviation of the
 Blockly.LANG_MATH_ONLIST_TOOLTIP_RANDOM = 'Return a random element from the list.';
 
 Blockly.LANG_MATH_CONSTRAIN_HELPURL = 'http://en.wikipedia.org/wiki/Clamping_%28graphics%29';
-Blockly.LANG_MATH_CONSTRAIN_INPUT_CONSTRAIN = 'constrain';
-Blockly.LANG_MATH_CONSTRAIN_INPUT_LOW = 'between (low)';
-Blockly.LANG_MATH_CONSTRAIN_INPUT_HIGH = 'and (high)';
+Blockly.LANG_MATH_CONSTRAIN_INPUT_CONSTRAIN = 'borner';
+Blockly.LANG_MATH_CONSTRAIN_INPUT_LOW = 'entre (mini)';
+Blockly.LANG_MATH_CONSTRAIN_INPUT_HIGH = 'et (maxi)';
 Blockly.LANG_MATH_CONSTRAIN_TOOLTIP = 'Constrain a number to be between the specified limits (inclusive).';
 
 Blockly.LANG_MATH_MODULO_HELPURL = 'http://en.wikipedia.org/wiki/Modulo_operation';
@@ -337,11 +337,11 @@ Blockly.LANG_TEXT_PROMPT_HELPURL = 'http://www.liv.ac.uk/HPC/HTMLF90Course/HTMLF
 Blockly.LANG_TEXT_PROMPT_TITLE_PROMPT_FOR = 'prompt for';
 Blockly.LANG_TEXT_PROMPT_TITILE_WITH_MESSAGE = 'with message';
 Blockly.LANG_TEXT_PROMPT_TOOLTIP = 'Prompt for user input with the specified text.';
-Blockly.LANG_TEXT_PROMPT_TYPE_TEXT = 'texte';
-Blockly.LANG_TEXT_PROMPT_TYPE_NUMBER = 'nombre';
+Blockly.LANG_TEXT_PROMPT_TYPE_TEXT = 'text';
+Blockly.LANG_TEXT_PROMPT_TYPE_NUMBER = 'number';
 
 // Lists Blocks.
-Blockly.LANG_CATEGORY_LISTS = 'Lists';
+Blockly.LANG_CATEGORY_LISTS = 'Listes';
 Blockly.LANG_LISTS_CREATE_EMPTY_HELPURL = 'http://en.wikipedia.org/wiki/Linked_list#Empty_lists';
 Blockly.LANG_LISTS_CREATE_EMPTY_TITLE = 'create empty list';
 Blockly.LANG_LISTS_CREATE_EMPTY_TOOLTIP = 'Returns a list, of length 0, containing no data records';
