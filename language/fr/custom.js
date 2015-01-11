f7219b5922e21d5964c8540b681e2476bed6abaa
SebCanet <sebastien.canet@ac-nantes.fr>
2015-01-11 17:24:45 +0100

    Nettoyage

--- language/fr/custom.js
+++ language/fr/custom.js
@@ -7,32 +7,6 @@
 //define blocks
 if (!Blockly.Language) Blockly.Language = {};
 
-//define read block
-Blockly.Language.custom_read = {
-  category: 'Custom',
-  helpUrl: '',
-  init: function() {
-    this.setColour(230);
-	this.appendDummyInput("")
-	    .appendTitle("CustomRead PIN#")
-	    .appendTitle(new Blockly.FieldDropdown(profile.default.digital), "PIN");
-    this.setOutput(true, Boolean);
-    this.setTooltip('input block');
-  }
-};
-
-Blockly.Language.custom_read = {
-  category: 'Custom',
-  helpUrl: '',
-  init: function() {
-    this.setColour(230);
-	this.appendDummyInput("")
-	    .appendTitle("CustomRead PIN#")
-	    .appendTitle(new Blockly.FieldDropdown(profile.default.digital), "PIN");
-    this.setOutput(true, Boolean);
-    this.setTooltip('input block');
-  }
-};
 
 //define write block
 Blockly.Language.initbot = {
@@ -195,13 +169,6 @@ Blockly.Language.humidite = {
 };
 
 // define generators
-Blockly.Arduino.custom_write = function() {
-  var dropdown_pin = this.getTitleValue('PIN');
-  var value_num = Blockly.Arduino.valueToCode(this, 'NUM', Blockly.Arduino.ORDER_ATOMIC);
-
-  var code = 'analogWrite('+dropdown_pin+','+value_num+');\n';
-  return code;
-};
 
 Blockly.Arduino.initbot = function() {
   var dropdown_pin = this.getTitleValue('PIN');
@@ -708,15 +675,4 @@ Blockly.Arduino.blinkybot = function() {
   
   var code = 'blinkybot();\n';
   return code;
-};
-
-Blockly.Arduino.custom_read = function() {
-  var dropdown_pin = this.getTitleValue('PIN');
-  
-  // Blockly.Arduino.definitions_['define_custom_read'] = '#include &lt;Servo.h&gt;\n';
-  // Blockly.Arduino.definitions_['var_custom_read'+dropdown_pin] = 'Servo servo_'+dropdown_pin+';\n';
-  // Blockly.Arduino.setups_['setup_custom_read_'+dropdown_pin] = 'servo_'+dropdown_pin+'.attach('+dropdown_pin+');\n';
-  
-  var code = 'analogRead('+dropdown_pin+')';
-  return [code, Blockly.Arduino.ORDER_ATOMIC];
 };
